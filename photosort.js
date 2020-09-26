769af7f6ff9a730e0ee807ce51d46a0c952e3bc7
Joel Hamilton <joelhamilton5@gmail.com>
2020-09-26 15:28:30 -0400

    add comments

--- photosort.js
+++ photosort.js
@@ -2,79 +2,52 @@ const path = require('path');
 const fs = require('fs');
 var moment = require('moment');
 
-//joining path of directory 
-const directoryPath = process.argv[2]; // first passed-in arg
+let directoryPath = process.argv[2]; // first passed-in arg
+let files = fs.readdirSync(directoryPath);
 
-//passsing directoryPath and callback function
-fs.readdir(directoryPath, function (err, files) {
-    //handling error
-    if (err) {
-        return console.log('Unable to scan directory: ' + err);
-    } 
-
-    //listing all files using forEach
-    files.forEach(function (file) {
+files.forEach(file => {
 	let filePath = path.join(directoryPath, file);
+	let stats = fs.statSync(filePath);
 
-	 fs.stat(filePath, function(err, stats) {
- 	    if (err) {
-	        return console.log('Unable to stat file: ' + err);
-	    } 
+	if(stats.isDirectory()) {
+		if(!filePath.toUpperCase().includes("IMG")) return;
 
-	 	if(stats.isDirectory()) {
-	 		// get each photo in dir, and if it is COVER, rename to dirname + '.jpg'
-	 		fs.readdirSync(filePath, function(err, files) {
-		 	    if (err) {
-	       			return console.log('Unable to scan directory: ' + err);
-	    		} 
+		let dirFiles = fs.readdirSync(filePath);
 
-	 			files.forEach (file => {
-	 				if(file.contains('COVER')) {
-	 					let oldPath = path.join(filePath, file);
-	 					let newPath = filePath + '.jpg';
-	 					fs.renameSync(oldPath, newPath);
-	 					moveToMonth(newPath);
-	 				}
-	 			});
-	 		});
+        // covers are the google portrait-mode blurred version
+		let covers = dirFiles.filter(dirFile => dirFile.includes('COVER'));
 
-	 		// if this isn't a portrait directory, we'll leave it alone
-			if(files.length <= 1) {
-				fs.rmDir(filePath);
-			}
-	 	} else {
-	 		moveToMonth(filePath);
-	 	}
-	  });  
-    });
+		if(covers.length === 1) { // only one cover, we'll save it
+			let oldPath = path.join(filePath, covers[0]);
+            
+            // rename to the folder name (will end up in parent dir)
+			let newPath = filePath + '.jpg';
+			fs.renameSync(oldPath, newPath);
+			fs.rmdirSync(filePath, {recursive: true});
+			moveToMonth(newPath);
+		} else if (!dirFiles.length) { // multiple covers, choose manually
+			fs.rmdirSync(filePath, {recursive: true});
+		}
+	} else {
+		moveToMonth(filePath);
+	}
 });
 
 function moveToMonth(filePath) {
-	let match = filePath.match(/_\d{7}_/);
-	if(!match.length) return;
+	let match = filePath.match(/[_-]\d{8}[_-]/);
+	if(!match) {
+		console.log("No match on file: " + filePath);
+		return;
+	}
 
-	let dateString = match[0]substr(1, 8);
+	let dateString = match[0].substr(1, 8);
 	let date = moment(dateString);
+	let fileName = path.basename(filePath);
 
 	let monthDir = path.join(directoryPath, date.format("MM - MMMM"));
 	if(!fs.existsSync(monthDir)) {
-		// TODO problems with async fn calling this
-		fs.mkdir('/tmp/a/apple', { recursive: true }, (err) => {
-		  if (err) throw err;
-		});
+		fs.mkdirSync(monthDir);
 	}
+
+	fs.renameSync(filePath, path.join(monthDir, fileName));
 }
-
-
-/**
-
-for each dir/file
-
-- if folder, rename photo inside folder that contains COVER to correct format, 
-move it to the correct month
-	- delete folder, if 0 or 1 remaining photos
-- if file, move to correct folder
-
-
-*/
-
